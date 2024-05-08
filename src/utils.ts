baaa74c90b2aa7e151e0866ff542265308035508
Anthony Fu <anthonyfu117@hotmail.com>
2024-05-08 02:45:54 +0200

    refactor: use shared utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,46 +1,9 @@
 import type { Linter } from 'eslint'
+import { unindent } from '@antfu/utils'
 import type { NormalizedTestCase, RuleModule, TestCase, TestCaseError } from './types'
 import { interpolate } from './vendor/interpolate'
 
-const reFullWs = /^\s*$/
-
-/**
- * Remove common leading whitespace from a template string.
- *
- * @example
- * ```ts
- * const str = unindent`
- *   if (a) {
- *     b()
- *   }
- * `
- */
-export function unindent(str: TemplateStringsArray) {
-  const lines = str[0].split('\n')
-  const whitespaceLines = lines.map(line => reFullWs.test(line))
-
-  const commonIndent = lines
-    .reduce((min, line, idx) => {
-      if (whitespaceLines[idx])
-        return min
-      const indent = line.match(/^\s*/)?.[0].length
-      return indent === undefined ? min : Math.min(min, indent)
-    }, Number.POSITIVE_INFINITY)
-
-  let emptyLinesHead = 0
-  while (emptyLinesHead < lines.length && whitespaceLines[emptyLinesHead])
-    emptyLinesHead++
-  let emptyLinesTail = 0
-  while (emptyLinesTail < lines.length && whitespaceLines[lines.length - emptyLinesTail - 1])
-    emptyLinesTail++
-
-  return lines
-    .slice(emptyLinesHead, lines.length - emptyLinesTail)
-    .map(line => line.slice(commonIndent))
-    .join('\n')
-}
-
-export { unindent as $ }
+export { unindent, unindent as $ }
 
 export function normalizeTestCase(c: TestCase, type?: 'valid' | 'invalid'): NormalizedTestCase {
   const obj = typeof c === 'string'
