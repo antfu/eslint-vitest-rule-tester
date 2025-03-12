4b2367ee20ed21faa7605102f70b45e8db049387
Anthony Fu <github@antfu.me>
2025-03-12 12:41:59 +0800

    perf: optmize bundle

--- build.config.ts
+++ build.config.ts
@@ -4,9 +4,17 @@ export default defineBuildConfig({
   entries: [
     'src/index',
   ],
-  declaration: true,
+  declaration: 'node16',
   clean: true,
   rollup: {
-    inlineDependencies: true,
+    inlineDependencies: [
+      '@antfu/utils',
+      'ajv',
+      'ajv/lib/refs/json-schema-draft-04.json',
+      'fast-json-stable-stringify',
+      'fast-deep-equal',
+      'uri-js',
+      'json-schema-traverse',
+    ],
   },
 })
